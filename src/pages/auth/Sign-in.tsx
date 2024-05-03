fddbff0069039363b77c38956afee9524d524db0
Ruan Cardoso dos Santos <cardosoruan2001@gmail.com>
2024-05-03 14:29:03 -0300

    feat: Hook Form

--- src/pages/auth/Sign-in.tsx
+++ src/pages/auth/Sign-in.tsx
@@ -1,10 +1,28 @@
 import { Helmet } from 'react-helmet-async'
+import { useForm } from 'react-hook-form'
+import * as zod from 'zod'
 
 import { Button } from '../../components/ui/button'
 import { Input } from '../../components/ui/input'
 import { Label } from '../../components/ui/label'
 
+const signInForm = zod.object({
+  email: zod.string().email(),
+})
+
+type signInFormType = zod.infer<typeof signInForm>
+
 export function SignIn() {
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<signInFormType>()
+
+  function handleSignIn(data: signInFormType) {
+    console.log(data)
+  }
+
   return (
     <>
       <Helmet title="Login" />
@@ -20,13 +38,13 @@ export function SignIn() {
             </p>
           </div>
 
-          <form className="space-y-4">
+          <form onSubmit={handleSubmit(handleSignIn)} className="space-y-4">
             <div className="space-y-2">
               <Label htmlFor="email">Seu e-mail</Label>
-              <Input id="email" type="email" />
+              <Input id="email" type="email" {...register('email')} />
             </div>
 
-            <Button type="submit" className="w-full">
+            <Button disabled={isSubmitting} type="submit" className="w-full">
               Acessar painel
             </Button>
           </form>
