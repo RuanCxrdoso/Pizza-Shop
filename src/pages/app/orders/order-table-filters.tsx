7b600e234bc48a772ad665b16f092639e4d244e6
Ruan Cardoso dos Santos <cardosoruan2001@gmail.com>
2024-05-05 20:31:05 -0300

    feat: Pagination component

--- src/pages/app/orders/order-table-filters.tsx
+++ src/pages/app/orders/order-table-filters.tsx
@@ -1,7 +1,6 @@
 import { Search, X } from 'lucide-react'
 
-import { Button } from '@/components/ui/button'
-
+import { Button } from '../../../components/ui/button'
 import { Input } from '../../../components/ui/input'
 import {
   Select,
